3943fb6036b0a109bed38f43220a62d674ccd7ea
blobzik <159489673+blobzik@users.noreply.github.com>
2024-06-12 22:29:14 +0300

    resolvers & mutations

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryInput } from './dto/create-category.input';
 import { UpdateCategoryInput } from './dto/update-category.input';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
+  private repository: Category[]
+  
+  constructor() {
+    this.repository = [];
+  }
+
   create(createCategoryInput: CreateCategoryInput) {
-    return 'This action adds a new category';
+    this.repository.push(createCategoryInput);
+    return createCategoryInput;
   }
 
   findAll() {
-    return `This action returns all category`;
+    return this.repository;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    return this.repository.find(item => item.id === id);
   }
 
   update(id: number, updateCategoryInput: UpdateCategoryInput) {
-    return `This action updates a #${id} category`;
+    // return `This action updates a #${id} category`;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} category`;
+    // return `This action removes a #${id} category`;
   }
 }
