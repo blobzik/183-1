251a437f37cfec03ba597630bacd6beb3de4fce8
blobzik <159489673+blobzik@users.noreply.github.com>
2024-06-12 13:39:46 +0300

    Add files to progects

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Category } from './category/entities/category.entity';
+import { Post } from './posts/entities/post.entity';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME,
+      entities: [Category, Post, User],
+      synchronize: true,
+    }),
+    CategoryModule,
+    PostsModule,
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
